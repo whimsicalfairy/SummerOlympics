3aa71c9f68c3ded2884153dc7da6abaf363bc4bb
Betsabe Juarez <whimsicalfairy>
2026-04-21 14:19:28 -0700

    Initial Commit

---
+++ SummerOlympics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SummerOlympics
+//
+//  Created by JUAREZ HERNANDEZ, BETSABE on 4/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SummerOlympics/SummerOlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsApp.swift
+//  SummerOlympics
+//
+//  Created by JUAREZ HERNANDEZ, BETSABE on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct SummerOlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
